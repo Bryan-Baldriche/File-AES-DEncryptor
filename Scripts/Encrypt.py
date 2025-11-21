d4548c416ae9913b1ff22bfba7ffaa35796cdf60
Owlee <55449800+Bryan-Baldriche@users.noreply.github.com>
2025-11-21 10:09:25 -0500

    Update Encrypt.py
    Added code for user to input file or directory path
    Added code to determine if input path is a directory or file
    Added code to zip directory without compression if detected as such

--- Scripts/Encrypt.py
+++ Scripts/Encrypt.py
@@ -1,6 +1,61 @@
 import os
 import base64
+import shutil
+import tempfile
+import pathlib
+import secrets
+import zipfile
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
+
+### CRYPTO CONSTANTS
+
+KEY_LENGTH = 32
+SALT_LENGTH = 16
+NONCE_LENGTH = 12
+PBKDF2_ITERATIONS = 400000
+ZIP_TEMP_PREFIX = "ENC_TEMP_"
+
+### derive 256-bit key
+
+def derive_key(password: str, salt: bytes) -> bytes:
+  password_bytes = password.encode("utf-8")
+
+  kdf = PBKDF2HMAC(
+    algorithm=hshes.SHA256(),
+    length=KEY_LENGTH,
+    salt=salt,
+    iterations=PBKDF2_ITERATIONS,
+  )
+
+  key = kdf.derive(password_bytes)
+  return key
+
+### Detect file/directory and zip if directory
+
+def user_input_file(input_path: str) -> tuple[pathlib.Path, pathlib.Path | None]:
+
+  path_to_file = pathlib.Path(input_path)
+
+  if path_to_file.is_file():
+    return path_to_file, None
+  if not path_to_file.is_dir():
+    raise ValueError(f"Input path is neither a file nor directory: {input_path}")
+
+  temp_dir - pathlib.Path)tempfile.mkdtemp(prefix=ZIP_TEMP_PREFIX))
+
+  zip_path = temp_dir / (path_to_file.name +".zip")
+
+  # none-compressed zip for file integrity
+
+  with zipfile.ZipFile(zip_path, "w", compression=zipfile.ZIP_STORED) as z:
+    for file_path in path_to_file.rglob("*"):
+      if file_path.if_file():
+        z.write(
+          file_path,
+          file_path.relative_to(path_to_file)
+        )
+
+  return zip_path, temp_dir
